05fd22b43d8984d80fd947839b618803af7153cf
Takeru Ohta <phjgt308@gmail.com>
2017-10-09 23:59:28 +0900

    Add `SpanReceiver`

--- src/trace.rs
+++ src/trace.rs
@@ -8,6 +8,10 @@ pub struct Tracer<T> {
     span_tx: mpsc::Sender<FinishedSpan<T>>,
 }
 impl<T> Tracer<T> {
+    pub fn new() -> (Self, SpanReceiver<T>) {
+        let (tx, rx) = mpsc::channel();
+        (Tracer { span_tx: tx }, rx)
+    }
     pub fn span(&self, operation_name: Cow<'static, str>, state: T) -> SpanBuilder<T> {
         SpanBuilder::new(operation_name, state, self.span_tx.clone())
     }
@@ -17,3 +21,5 @@ impl<T> Clone for Tracer<T> {
         Tracer { span_tx: self.span_tx.clone() }
     }
 }
+
+pub type SpanReceiver<T> = mpsc::Receiver<FinishedSpan<T>>;
