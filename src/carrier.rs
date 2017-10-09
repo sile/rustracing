a46aa12a8b30632294678c2ff43e307a0827f905
Takeru Ohta <phjgt308@gmail.com>
2017-10-10 00:06:06 +0900

    Split `Carrier` into `Inject` and `Extract`

--- src/carrier.rs
+++ src/carrier.rs
@@ -1,6 +1,15 @@
+use std::error::Error;
+
 use span::SpanContext;
 
-pub trait Carrier<T> {
-    fn inject(&mut self, context: &SpanContext<T>);
-    fn extract(&mut self) -> Option<SpanContext<T>>;
+// TODO: struct NeverFails
+
+pub trait Inject<T> {
+    type Error: Error;
+    fn inject(&mut self, context: &SpanContext<T>) -> Result<(), Self::Error>;
+}
+
+pub trait Extract<T> {
+    type Error: Error;
+    fn extract(&mut self) -> Result<Option<SpanContext<T>>, Self::Error>;
 }
